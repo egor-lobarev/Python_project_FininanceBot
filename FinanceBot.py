afcd502d95069e8bdf229702c47d8658779ca5a3
egor.lobarev <egor.lobarev@gmail.com>
2023-04-22 09:38:15 +0300

    Расположение import'ов по PEP8

--- FinanceBot.py
+++ FinanceBot.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from datetime import timedelta
+import random
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import random
 
 # Заполнение значений по умолчанию
 
